0783f7ae7fe2e814d430325f526d650fd3f73686
VitorLima1998 <vitor.xlr8@gmail.com>
2023-09-09 21:45:12 -0400

    feat: add cors

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ const routes = require('./routes');
 const authRoutes = require('./routes/auth');
 const productRoutes = require('./routes/product');
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.use('/', routes);
