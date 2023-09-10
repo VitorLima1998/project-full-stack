ca9b057f8dca716e1c356e540392e1d077af6797
VitorLima1998 <vitor.xlr8@gmail.com>
2023-09-09 21:45:35 -0400

    feat: add method array images

--- controllers/productController.js
+++ controllers/productController.js
@@ -7,28 +7,38 @@ module.exports = {
   syncDatabase: async () => {
     const products = (await axios.get('https://dummyjson.com/products')).data;
 
-    products.products.map(async (product) => {
-      await prisma.product.create({
-        data: {
-          title: product.title,
-          description: product.description,
-          price: product.price,
-          discountPercentage: product.discountPercentage,
-          rating: product.rating,
-          stock: product.stock,
-          brand: product.brand,
-          category: product.category,
-          thumbnail: product.thumbnail,
-          images: product.images[0],
-        },
-      });
-    });
+    await Promise.all(
+      products.products.map(async (product) => {
+        await prisma.product.create({
+          data: {
+            title: product.title,
+            description: product.description,
+            price: product.price,
+            discountPercentage: product.discountPercentage,
+            rating: product.rating,
+            stock: product.stock,
+            brand: product.brand,
+            category: product.category,
+            thumbnail: product.thumbnail,
+            images: {
+              create: product.images.map((imageUrl) => ({
+                url: imageUrl,
+              })),
+            },
+          },
+        });
+      })
+    );
 
     return products;
   },
 
   getAll: async () => {
-    const products = await prisma.product.findMany();
+    const products = await prisma.product.findMany({
+      include: {
+        images: true,
+      },
+    });
     return products;
   },
 };
